5b1f9504655f3e1de5ecfde075af5e5f5933437f
patcoin-official <tonywoodtv@gmail.com>
2024-06-04 20:28:16 +0100

    Add files via upload

---
+++ patcoun.js
@@ -0,0 +1,29 @@
+
+// document.getElementById("decrease").onclick = function(){
+//   count-=7 
+//  document.getElementById("value").innerHTML = count;
+// }
+
+document.getElementById("start").onclick = function(){
+ count=3
+document.getElementById("value").innerHTML = count;
+}
+
+document.getElementById("increase").onclick = function(){
+ count+=200
+document.getElementById("value").innerHTML = count;
+if (value => 10000) {
+  alert ("you are all cought up!!")
+  document.getElementById("dcount").innerHTML = count.value
+}
+
+
+}
+
+// increase.addEventListner("click", ()=>{
+//   document.getElementById("value").innerHTML = count+4
+
+// if (end){
+//   value = "count";
+// }
+// });
