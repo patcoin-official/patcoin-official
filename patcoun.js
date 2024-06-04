5fb4c2705ecccf2a26eaddbb24bbbae606c0db84
patcoin-official <tonywoodtv@gmail.com>
2024-06-04 20:32:43 +0100

    Update patcoun.js

--- patcoun.js
+++ patcoun.js
@@ -13,7 +13,7 @@ document.getElementById("increase").onclick = function(){
  count+=200
 document.getElementById("value").innerHTML = count;
 if (value => 10000) {
-  alert ("you are all cought up!!")
+  // alert ("you are all cought up!!")
   document.getElementById("dcount").innerHTML = count.value
 }
 
